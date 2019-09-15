1a1334fd242997af73f1221c671d6a90de2ec4a6
陈海洋 <1559843332@qq.com>
2019-09-15 08:57:07 +0800

    simple todolist finish

--- README.md
+++ README.md
@@ -11,3 +11,26 @@
 ## 运行方式
 
 `npm start`
+
+## 踩坑
+
+1. ### setState
+`this.setState({`写成了`this.setState = ({`
+
+## 建议
+
+```
+  handleItemClick(index){
+    // 不推荐
+    // this.state.tdlist.splice(index,1);
+    // this.setState({
+    //   tdlist : this.state.tdlist
+    // })
+    // 推荐copy副本的方式
+    const list = [...this.state.tdlist];
+    list.splice(index,1);
+    this.setState({
+      tdlist:list
+    })
+  }
+```
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,17 +2,38 @@ import React from 'react';
 
 class TodoList extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      tdlist : []
+      tdlist: [],
+      inputValue: ''
     }
   }
 
+  handleItemClick(index){
+    // 不推荐
+    // this.state.tdlist.splice(index,1);
+    // this.setState({
+    //   tdlist : this.state.tdlist
+    // })
+    // 推荐copy副本的方式
+    const list = [...this.state.tdlist];
+    list.splice(index,1);
+    this.setState({
+      tdlist:list
+    })
+  }
 
-  handleBtnClick(){
-    this.setState = ({
-      tdlist : [...this.state.tdlist,'hello world']
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+
+  handleBtnClick() {
+    this.setState({
+      tdlist: [...this.state.tdlist, this.state.inputValue],
+      inputValue: ''
     })
   }
 
@@ -20,21 +41,20 @@ class TodoList extends React.Component {
     return (
       <div>
         <div>
-          <input />
+          <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
           <button onClick={this.handleBtnClick.bind(this)}>add</button>
         </div>
         <ul>
           {
-            this.state.tdlist.map((it,index) => {
-              return <li key={index}>{it}</li>
+            this.state.tdlist.map((it, index) => {
+              return <li key={index} onClick={this.handleItemClick.bind(this,index)}>{it}</li>
             })
           }
         </ul>
       </div>
     );
   }
- 
+
 }
 
 export default TodoList;
- 
